ac36ff1ea5ea20b4fb09ac9805f9dc3066f40db8
Benjaunocerouno <vbenjamintr@gmail.com>
2026-07-02 15:34:43 -0500

    trabjo

---
+++ src/test/java/Laboratorio/Trabajo/TrabajoApplicationTests.java
@@ -0,0 +1,13 @@
+package Laboratorio.Trabajo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrabajoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
